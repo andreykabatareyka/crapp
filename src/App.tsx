be531cb856a15d44216a11879915df5574fdc621
drews <andrewfreeman1410@gmail.com>
2022-02-19 13:17:20 +0300

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-export default () =>{
+export default () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
+        <a>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
         </a>
       </header>
     </div>
-  )
-}
+  );
+};
